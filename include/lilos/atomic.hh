5bc72092a297b84324e004fb81aad374511e0955
Cliff L. Biffle <cbiffle@gmail.com>
2011-04-02 20:20:35 -0700

    Improved comments in atomic.hh.

--- include/lilos/atomic.hh
+++ include/lilos/atomic.hh
@@ -1,8 +1,24 @@
 #ifndef LILOS_ATOMIC_HH_
 #define LILOS_ATOMIC_HH_
 
+/*
+ * An alternative to the official <util/atomic.h> header.  Currently it just
+ * provides shorthand.
+ */
+
 #include <util/atomic.h>
 
+/*
+ * Disables interrupts and restores the previous state (disabled or not) at
+ * completion.  Slightly more expensive than a straight cli/sei pair, but more
+ * composable: your code need not care whether it was called with interrupts
+ * enabled.
+ *
+ * Example of use:
+ *  ATOMIC {
+ *    do_work();
+ *  }
+ */
 #define ATOMIC ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
 
 #endif  // LILOS_ATOMIC_HH_
